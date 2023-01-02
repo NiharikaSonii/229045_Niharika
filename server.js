9b6dfd3506f3074a9437e925090f3ca192ca52b2
rohit <rohitgadakh31@gmail.com>
2023-01-02 17:06:59 +0530

    file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app=express();
+
+const path=require("path");
+
+var staticFolder=express.static(path.join(__dirname,"public"))
+app.use(staticFolder);
+ app.get("/",function(req,res){
+     res.sendFile(path.join(__dirname,"/public/index.html"))
+ });
+app.listen(5000);
+console.log("server is running on this port number...")
